7f246b496f51085d6eeeac8775caa6a8a6354063
Daniel Miranda <developermiranda@gmail.com>
2021-04-19 20:03:37 -0300

    first commit

---
+++ lib/core/app_widget.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "DevQuiz",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text("NLW5 Flutter"),
+          ),
+        ),
+        body: Center(
+            child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Text(
+                  "Olá, Daniel Miranda",
+                  style: TextStyle(fontSize: 30, color: Colors.white),
+                ))),
+        floatingActionButton:
+            FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
+      ),
+    );
+  }
+}
