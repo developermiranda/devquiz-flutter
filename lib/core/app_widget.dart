0831e0c822b9757211098033e08eb821033d94e1
Daniel Miranda <developermiranda@gmail.com>
2021-04-19 21:22:38 -0300

    add page splash

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,3 +1,4 @@
+import 'package:devquiz/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
@@ -5,24 +6,7 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "DevQuiz",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Center(
-            child: Text("NLW5 Flutter"),
-          ),
-        ),
-        body: Center(
-            child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.purple,
-                    borderRadius: BorderRadius.circular(10)),
-                child: Text(
-                  "Olá, Daniel Miranda",
-                  style: TextStyle(fontSize: 30, color: Colors.white),
-                ))),
-        floatingActionButton:
-            FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
-      ),
+      home: SplashPage(),
     );
   }
 }
